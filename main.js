69955a331fa46fd54198fe4c56db1391282a7519
Hashan Athurugiriya <hashan.athurugiriya@gmail.com>
2024-12-05 14:30:54 +0530

    L2 - Finalized Commit

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = "Socks";
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus eveniet similique, autem praesentium eos tempore necessitatibus officiis nulla aut, corporis dolores asperiores magnam quo quia laudantium, quas veritatis voluptatem inventore?",
+    };
+  },
+});
