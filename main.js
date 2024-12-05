e7e7a2b0e64e0238d18fabe0e9badc19dd16b336
Hashan Athurugiriya <hashan.athurugiriya@gmail.com>
2024-12-05 14:33:14 +0530

    L2 - Branching to a new

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: "Socks",
+      product: "Shoes",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus eveniet similique, autem praesentium eos tempore necessitatibus officiis nulla aut, corporis dolores asperiores magnam quo quia laudantium, quas veritatis voluptatem inventore?",
     };
