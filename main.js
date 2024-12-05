6b54327cc5382724617c24105bf15151d7736ad1
Hashan Athurugiriya <hashan.athurugiriya@gmail.com>
2024-12-05 23:13:51 +0530

    L5 - Challenge completed

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "blue" },
+        { id: 2234, color: "green" },
+      ],
+      sizes: [
+        { id: 1111, code: "XS" },
+        { id: 1112, code: "S" },
+        { id: 1113, code: "M" },
+        { id: 1114, code: "L" },
+        { id: 1115, code: "XL" },
+      ],
+    };
+  },
+});
