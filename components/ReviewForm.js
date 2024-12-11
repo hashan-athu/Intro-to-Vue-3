60a0c61755ce5de897255b9af604da7be7a05496
Hashan Athurugiriya <hashan.athurugiriya@gmail.com>
2024-12-11 21:47:21 +0530

    L11 - Challenge partial 2

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -1,13 +1,13 @@
-app.component('review-form',{
-    template:
+app.component("review-form", {
+  template:
     /*html*/
-  `<form class="review-form" @submit.prevent="onSubmit">
+    `<form class="review-form" @submit.prevent="onSubmit">
   <h3>Leave a review</h3>
   <label for="name">Name:</label>
   <input id="name" v-model="name">
 
   <label for="review">Review:</label>      
-  <textarea id="review" v-mode="review"></textarea>
+  <textarea id="review" v-model="review"></textarea>
 
   <label for="rating">Rating:</label>
   <select id="rating" v-model.number="rating">
@@ -18,26 +18,41 @@ app.component('review-form',{
     <option>1</option>
   </select>
 
+    <label for="recommend">Would you recommend this product?</label>
+    <select id="recommend" v.model="recommend">
+        <option value="Yes" selected>Yes</option>
+        <option value="No">No</option>
+    </select>
+
   <input class="button" type="submit" value="Submit">
 </form>`,
-data(){
-    return{
-        name: '',
-        review: '',
-        rating: null
-    }
-},
-methods:{
-    onSubmit(){
-        let productReview = {
-            name: this.name,
-            review: this.review,
-            rating: this.rating
-        }
+  data() {
+    return {
+      name: "",
+      review: "",
+      rating: null,
+      recommend: ''
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.name === "" || this.review === "" || this.rating === null) {
+        alert("Please fill out all the fields before adding a review");
+        return;
+      }
 
-        this.name= ''
-        this.review= ''
-        this.rating= null
-    }
-}
-})
+      let productReview = {
+        name: this.name,
+        review: this.review,
+        rating: this.rating,
+        recommend: this.recommend
+      };
+      this.$emit("review-submitted", productReview);
+
+      this.name = "";
+      this.review = "";
+      this.rating = null;
+      this.recommend = ''
+    },
+  },
+});
